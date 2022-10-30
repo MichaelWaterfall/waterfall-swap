e144c0ec1a44d73a6c776bc3a98d9d41a458ed06
Michael Waterfall <michaeljames.waterfall@gmail.com>
2022-10-30 10:39:33 +0000

    test init function

--- test/dex.js
+++ test/dex.js
@@ -1,8 +1,19 @@
-/*const Dex = artifacts.require('Dex');
+const { expectRevert } = require("@openzeppelin/test-helpers");
 
-contract('Dex', () => {
+const Dex = artifacts.require("Dex.sol");
+const Dai = artifacts.require("mocks/Dai.sol");
+
+contract('Dex', (accounts) => {
+    const [liquidityPoolDai] = [accounts[1]];
     it('should deploy contract', async () => {
         const dex = await Dex.deployed();
         assert(dex.address != '');
     });
-});*/
+    //initialize function 
+    it('should create liquidity pool if liquidity does NOT already exist', async () => {
+        const dex = await Dex.deployed();
+        await dex.init(1);
+        const result = await dex.totalLiquidity(0);
+        assert(result == 0);
+    });
+});
